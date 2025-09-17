89d4453356e89f10ce79a6122c44db3ce7802b05
Sgross1 <shlgrs@gmail.com>
2025-09-17 23:32:01 +0300

    change background , header , buttobns

--- script.js
+++ script.js
@@ -108,3 +108,44 @@ $(document).on('click', '.card', function() {
 $('.navbar-brand').on('click', function() {
     $('html, body').animate({ scrollTop: 0 }, 600);
 });
+
+// רקע מתחלף
+const bgImages = [
+    "https://images.unsplash.com/photo-1503736334956-4c8f8e92946d?auto=format&fit=crop&w=1500&q=80", // 1
+    "https://images.unsplash.com/photo-1508974239320-0a029497e820?auto=format&fit=crop&w=1500&q=80", // 2
+    "https://images.unsplash.com/photo-1485291571150-772bcfc10da5?auto=format&fit=crop&w=1500&q=80", // 3
+    "https://images.unsplash.com/photo-1531850959096-cfbb6f26c5a8?auto=format&fit=crop&w=1500&q=80"  // 4
+];
+let bgIndex = 0;
+const bgDiv = document.getElementById('background-slideshow');
+
+// הגדרת תמונה ראשונה
+bgDiv.style.backgroundImage = `url('${bgImages[bgIndex]}')`;
+bgDiv.style.opacity = 1;
+
+function fadeToNextBg() {
+    bgDiv.style.transition = 'opacity 2s';
+    bgDiv.style.opacity = 0;
+    setTimeout(() => {
+        bgIndex = (bgIndex + 1) % bgImages.length;
+        bgDiv.style.backgroundImage = `url('${bgImages[bgIndex]}')`;
+        bgDiv.style.opacity = 1;
+    }, 2000);
+}
+
+// מעבר כל 10 שניות (כולל 2 שניות מעבר)
+setInterval(fadeToNextBg, 20000);
+
+$('#infoBtn').on('click', function() {
+    $('#infoBox').addClass('show');
+});
+$('#closeInfo').on('click', function() {
+    $('#infoBox').removeClass('show');
+});
+
+$('#aboutBtn').on('click', function() {
+    $('#aboutBox').addClass('show');
+});
+$('#closeAbout').on('click', function() {
+    $('#aboutBox').removeClass('show');
+});
